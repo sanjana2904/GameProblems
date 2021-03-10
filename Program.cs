7c3a08667b93a380ab4fffded0b206ae787d352f
sanjana k <sanjanarthi@gmail.com>
2021-03-10 12:31:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gaming");
+        }
+    }
+}
